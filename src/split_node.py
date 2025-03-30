dd3ba846e7370bace6aaa2a56be04ffd18fc251b
coding920 <justin.tamez04@gmail.com>
2025-03-29 19:45:40 -0700

    Added split node funcs for Image and Links

--- src/split_node.py
+++ src/split_node.py
@@ -14,11 +14,13 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type) -> list[TextNode]:
             raise Exception(f"invalid markdown syntax, missing closing {delimiter}")
 
         node_splits = node.text.split(delimiter)
-        for idx, split in enumerate(node_splits):
-            if idx % 2 == 0:
-                new_nodes.append(TextNode(split, TextType.Normal))
+        for split in node_splits:
+            if not split:
+                continue    
+            if split.startswith(" ") or split.endswith(" "):
+                new_nodes.append(TextNode(text=split, text_type=TextType.Normal))
             else:
-                new_nodes.append(TextNode(split, text_type))
+                new_nodes.append(TextNode(text=split, text_type=text_type))
 
     return new_nodes
 
@@ -31,3 +33,36 @@ def configure_extracter(regex):
 extract_md_images = configure_extracter(r"!\[(.*?)\]\((.*?)\)")
 extract_md_links = configure_extracter(r"\[(.*?)\]\((.*?)\)")
 
+def split_nodes_configurer(function, text_type):
+    def split_nodes(old_nodes) -> list[TextNode]:
+        new_nodes = []
+        for node in old_nodes:
+            if function(node.text) == []:
+                new_nodes.append(node)
+                continue
+            
+            # Split text on info
+            image_info = function(node.text)
+            for pair in image_info:
+                sections = node.text.split(f"{"!" if text_type == TextType.Image else ""}[{pair[0]}]({pair[1]})", 1)
+                sections = list(filter(lambda item: item if not item.isspace() else "", sections))
+                sections = list(filter(lambda item: item, sections))
+                if len(sections) == 1 and sections[0].startswith(" "):
+                    new_nodes.extend(
+                            [TextNode(text=pair[0], text_type=text_type, url=pair[1]),
+                             TextNode(text=sections[0], text_type=TextType.Normal)])
+                elif len(sections) < 2 and sections[0].endswith(" "):
+                    new_nodes.extend([
+                        TextNode(text=sections[0], text_type=TextType.Normal),
+                        TextNode(text=pair[0], text_type=text_type, url=pair[1])])
+                else:
+                    new_nodes.extend([
+                        TextNode(text=sections[0], text_type=TextType.Normal),
+                        TextNode(text=pair[0], text_type=text_type, url=pair[1]),
+                        TextNode(text=sections[1], text_type=TextType.Normal)])
+
+        return new_nodes
+    return split_nodes
+
+split_nodes_images = split_nodes_configurer(extract_md_images, TextType.Image)
+split_nodes_links = split_nodes_configurer(extract_md_links, TextType.Link)
