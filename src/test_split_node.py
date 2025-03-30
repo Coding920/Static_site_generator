5340035511b0421ef3db8b0f5fd9364ca9268ba0
coding920 <justin.tamez04@gmail.com>
2025-03-29 19:46:04 -0700

    Added unit test for funcs

--- src/test_split_node.py
+++ src/test_split_node.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from split_node import split_nodes_delimiter, extract_md_images, extract_md_links
+from split_node import split_nodes_delimiter, extract_md_images, extract_md_links, split_nodes_images, split_nodes_links
 from textnode import TextNode, TextType
 
 class split_node_test(TestCase):
@@ -42,7 +42,24 @@ class split_node_test(TestCase):
                 f"{nodes}", 
         "[TextNode(This is a normal block of text, normal, None)]"
         )
-        
+
+    def test_split_with_code_at_start_or_end(self):
+        node = TextNode(text="`code block` at the beginning", text_type=TextType.Normal)
+        nodes = split_nodes_delimiter([node], "`", TextType.Code)
+        self.assertEqual(
+                nodes,
+        [TextNode(text="code block", text_type=TextType.Code), TextNode(text=" at the beginning", text_type=TextType.Normal)]
+          )
+
+        node = TextNode(text="at the end `code block`", text_type=TextType.Normal)
+        nodes = split_nodes_delimiter([node], "`", TextType.Code)
+        self.assertEqual(
+                nodes,
+        [TextNode(text="at the end ", text_type=TextType.Normal), TextNode(text="code block", text_type=TextType.Code)]
+          )
+
+        #Identify special nodes with space
+
     # Test Extract Markdown Image func
     def test_extract_md_images(self):
         text = "![alt text](url text) ![second alt text](second url text)"
@@ -102,3 +119,80 @@ class split_node_test(TestCase):
                 link_info,
             [('anchor text', 'url text'), ('second anchor text', 'second url text')]
                )
+
+    # Test Split nodes images
+    def test_split_nodes_images(self):
+        node = TextNode(text="This is an image link ![alt text](url) in the middle", text_type=TextType.Normal)
+        new_nodes = split_nodes_images([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="This is an image link ", text_type=TextType.Normal), TextNode(text="alt text", text_type=TextType.Image, url="url"),\
+                    TextNode(text=" in the middle", text_type=TextType.Normal)]
+            )
+
+        node = TextNode(text="This is an image link at the end ![alt text](url)", text_type=TextType.Normal)
+        new_nodes = split_nodes_images([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="This is an image link at the end ", text_type=TextType.Normal), TextNode(text="alt text", text_type=TextType.Image, url="url")]
+            )
+
+        node = TextNode(text="![alt text](url) this is an image link at the beginning", text_type=TextType.Normal)
+        new_nodes = split_nodes_images([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="alt text", text_type=TextType.Image, url="url"), TextNode(text=" this is an image link at the beginning", text_type=TextType.Normal)]
+            )
+
+        node = TextNode(text=" ![alt text](url) this is an image link at the beginning", text_type=TextType.Normal)
+        new_nodes = split_nodes_images([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="alt text", text_type=TextType.Image, url="url"), TextNode(text=" this is an image link at the beginning", text_type=TextType.Normal)]
+            )
+
+        node = TextNode(text="![alt text](url) this is an image link at the beginning ", text_type=TextType.Normal)
+        new_nodes = split_nodes_images([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="alt text", text_type=TextType.Image, url="url"), TextNode(text=" this is an image link at the beginning ", text_type=TextType.Normal)]
+            )
+
+    # Test Split nodes images
+    def test_split_nodes_links(self):
+        node = TextNode(text="This is an image link [alt text](url) in the middle", text_type=TextType.Normal)
+        new_nodes = split_nodes_links([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="This is an image link ", text_type=TextType.Normal), TextNode(text="alt text", text_type=TextType.Link, url="url"),\
+                    TextNode(text=" in the middle", text_type=TextType.Normal)]
+            )
+
+        node = TextNode(text="This is an image link at the end [alt text](url)", text_type=TextType.Normal)
+        new_nodes = split_nodes_links([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="This is an image link at the end ", text_type=TextType.Normal), TextNode(text="alt text", text_type=TextType.Link, url="url")]
+            )
+
+        node = TextNode(text="[alt text](url) this is an image link at the beginning", text_type=TextType.Normal)
+        new_nodes = split_nodes_links([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="alt text", text_type=TextType.Link, url="url"), TextNode(text=" this is an image link at the beginning", text_type=TextType.Normal)]
+            )
+
+        node = TextNode(text=" [alt text](url) this is an image link at the beginning", text_type=TextType.Normal)
+        new_nodes = split_nodes_links([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="alt text", text_type=TextType.Link, url="url"), TextNode(text=" this is an image link at the beginning", text_type=TextType.Normal)]
+            )
+
+        node = TextNode(text="[alt text](url) this is an image link at the beginning ", text_type=TextType.Normal)
+        new_nodes = split_nodes_links([node])
+        self.assertEqual(
+                new_nodes,
+            [TextNode(text="alt text", text_type=TextType.Link, url="url"), TextNode(text=" this is an image link at the beginning ", text_type=TextType.Normal)]
+            )
+
