bcfe371dd39b0cc27deebfe085e9bbc8fe394019
coding920 <justin.tamez04@gmail.com>
2025-04-01 13:37:00 -0700

    Made changes to main.sh to refresh public dir when program is run

--- src/main.py
+++ src/main.py
@@ -1,7 +1,35 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 def main():
-    test = TextNode("this is some anchor text", TextType.Link, "https://www.boot.dev")
-    print(test)
+    if not os.path.exists("./public"):
+        os.mkdir("./public")
+    elif os.path.isfile("./public"):
+        os.remove("./public")
+        os.mkdir("./public")
+    elif os.listdir("./public"):
+        shutil.rmtree("./public")
+        os.mkdir("./public")
+
+    if not os.path.exists("./static"):
+        raise ValueError("static dir missing")
+    copy_files("./static", "./public")
+
+    return
+
+
+def copy_files(src, dest):
+    old_files = os.listdir(src)
+    for file in old_files:
+        file_path = f"{src}/{file}"
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, f"{dest}/{file}")
+        else:
+            sub_dir = f"{src}/{file}"
+            sub_dest = f"{dest}/{file}"
+            os.mkdir(sub_dest)
+            copy_files(sub_dir, sub_dest)
+    return
+    
 
 main()
